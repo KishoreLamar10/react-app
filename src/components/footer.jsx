742afdfc4d7ae1f5bdc0721bab83eebb35a7aa8e
Lamar <balakishore619@gmail.com>
2024-05-11 10:36:05 +0530

    inital commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/footer.css'
+
+function footer(){
+    return(
+        <div className="footer">
+            <p>Created by Lamar </p>
+        </div>
+    )
+}
+
+export default footer;
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/header.css'
+
+function header(){
+    return(
+        <div className="header">
+            <h1>MY RESUME</h1>
+        </div>
+    )
+}
+
+export default header;
